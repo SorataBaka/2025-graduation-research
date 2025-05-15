3280e9fed2fcc00b3cf08238e26e985130e002b8
Christian Harjuno <harjuno.tian@gmail.com>
2025-05-15 15:46:29 +0900

    added logging model

---
+++ twitter-parse-v2/src/database/log_model.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+	smallest_date: {
+		type: Date,
+		required: true,
+	},
+	started_at: {
+		type: Date,
+		required: true,
+	},
+});
+
+export default mongoose.model("logs_database", schema);
